c03287b9daa48a23152f1f0b6363aa65bdc39e79
Noam Schwartz <noamschwartz1@gmail.com>
2019-05-28 10:32:07 +0300

    Add project files.

---
+++ Ex3/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Ex3
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            routes.MapRoute("display", "display/{ip}/{port}",
+            defaults: new { controller = "Info", action = "display" });
+
+            routes.MapRoute("save", "save/{ip}/{port}/{timesPerSec}/{seconds}/{fileName}",
+            defaults: new { controller = "Info", action = "save" });
+        
+        
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Info", action = "info", id = UrlParameter.Optional });
+                
+
+    }
+    
+    }
+}
---
+++ Ex3/Controllers/InfoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ex3.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult display(string ip, int port)
+        {
+            //write stuff
+            //create client
+            //open client server
+            //pass lon and lat
+            //disconnect
+            return View();
+        }
+        
+        
+        [HttpGet]
+        public ActionResult save()
+        {
+            //write stuff
+
+            return View();
+        }
+        
+    }
+}
